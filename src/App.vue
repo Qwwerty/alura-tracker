7236555128c34945e57f3be75621cb7e403dd570
Rhalf Oliveira <rhalfoliveira@teksystem.com.br>
2022-01-07 16:52:40 -0300

    fisrt commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <main columns is-gapless is-multiline>
+    <div class="column is-one-quarter">
+
+    </div>
+    <div class="column is-three-quarter">
+
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style></style>
