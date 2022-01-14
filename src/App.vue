69a68793db8a2cd21e1eb36aecadcf6b482f15c8
Rhalf Oliveira <rhalfoliveira@teksystem.com.br>
2022-01-14 08:12:56 -0300

    feature: criar função de inicialização do cronometro

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <main columns is-gapless is-multiline>
+  <main class="columns is-gapless is-multiline">
     <div class="column is-one-quarter">
-
+      <BarraLateral />
     </div>
     <div class="column is-three-quarter">
-
+      <Formulario />
     </div>
   </main>
 </template>
@@ -12,8 +12,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import BarraLateral from './components/BarraLateral.vue'
+import Formulario from './components/Formulario.vue'
+
 export default defineComponent({
   name: "App",
+
+  components: { BarraLateral, Formulario }
 });
 </script>
 
---
+++ src/components/Formulario.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-8" role="form" aria-label="Formulário para criação de uma tarefa">
+        <input class="input" type="text" placeholder="Qual tarefa você deseja iniciar?">
+      </div>
+      <div class="column">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <section>
+            <strong>{{ tempoDecorrido }}</strong>
+          </section>
+          <button class="button" @click="iniciar">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+
+          <button class="button" @click="finalizar">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Formulario',
+
+  data: () => ({
+    tempoEmSegundos: 0,
+  }),
+
+  computed: {
+    tempoDecorrido: function(): string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
+    }
+  },
+
+  methods: {
+    iniciar: function(): void {
+      setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+    },
+
+    finalizar: function() {
+      console.log('finalizando');
+    }
+  }
+})
+</script>
