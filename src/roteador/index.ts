62bbad01eb3cd820af4675ae50b6f64b32ef872e
Rhalf Oliveira <rhalfoliveira@teksystem.com.br>
2022-01-18 08:40:17 -0300

    refactor: separar criação de projeto e tabela de exibição de projetos

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,11 +1,14 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 import Tarefas from "../views/Tarefas.vue";
-import Projetos from '../views/Projetos.vue';
+import Projetos from "../views/Projetos.vue";
+import Formulario from "../views/Projetos/Formulario.vue";
 
 const rotas: RouteRecordRaw[] = [
-  { path: '/', name: 'Tarefas', component: Tarefas },
-  { path: '/projetos', name: 'Projetos', component: Projetos }
+  { path: "/", name: "Tarefas", component: Tarefas },
+  { path: "/projetos", name: "Projetos", component: Projetos },
+  { path: "/projetos/novo", name: "Novo projeto", component: Formulario },
+  { path: "/projetos/:id", name: "Editar projeto", component: Formulario },
 ];
 
 const roteador = createRouter({
--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -2,31 +2,32 @@
   <section class="projetos">
     <h1 class="title">Projetos</h1>
 
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label for="nomeDoProjeto" class="label">Nome do projeto</label>
-        <input
-          type="text"
-          class="input"
-          v-model="nomeDoProjeto"
-          id="nomeDoProjeto"
-        />
-      </div>
-      <div class="field">
-        <button class="button" type="submit">Salvar</button>
-      </div>
-    </form>
+    <router-link :to="{ name: 'Novo projeto' }" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo Projeto</span>
+    </router-link>
+
     <table class="table is-fullwidth">
       <thead>
         <tr>
           <th>ID</th>
           <th>Nome</th>
+          <th>Ações</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="projeto in projetos" :key="projeto.id">
           <td>{{ projeto.id }}</td>
           <td>{{ projeto.nome }}</td>
+          <td>
+            <router-link :to="`/projetos/${projeto.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </router-link>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -40,20 +41,9 @@ import { useStore } from "../store";
 export default defineComponent({
   name: "Projetos",
 
-  data: () => ({
-    nomeDoProjeto: "",
-  }),
-
-  methods: {
-    salvar: function () {
-      this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
-      this.nomeDoProjeto = "";
-    },
-  },
   setup: function () {
     const store = useStore();
     return {
-      store,
       projetos: computed(() => store.state.projetos),
     };
   },
